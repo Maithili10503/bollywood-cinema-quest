5a0fa040ef1ecfed1fceacbeaa3bb3751d4ce70c
Maithili Gohokar <maithili.g@dealermatix.com>
2025-09-02 15:53:37 +0530

    Final  commit

--- firebase_config.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-# Load credentials
-cred = credentials.Certificate("firebase_service_account.json")
-firebase_admin.initialize_app(cred)
-
-# Get Firestore DB instance
-db = firestore.client()
